5ef297aeea36b69acb7e225186d80605265c814e
alineiro <Adrian_Lineiro@protonmail.com>
2026-04-11 17:47:48 +0200

    its starting to look like something

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
 
+//CONSTRUCTORS GO HERE RIGHT? FOR NOW AT LEAST.
+
 class	Contact
 {
+	private:
+		char	f_name[10];
+		char	l_name[10];
+		char	nickname[10];
+		char	phone_number[10];
+		char	secret[10];
 	public:
 		Contact();
 		~Contact();
@@ -11,8 +19,13 @@ class	Contact
 class	PhoneBook
 {
 	private:
-		Contact	_contact;
+		Contact	_contact[8];
 	public:
 		PhoneBook();
 		~PhoneBook();
 };
+
+Contact::Contact()
+{
+	f_name[0] = '\0';
+	l_name[0] = '\0';
